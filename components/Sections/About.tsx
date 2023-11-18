92ac84a19266bb1ebbd8a7880e56b9ac85c22e29
joeljosjoseph <joeljosjoseph12@gmail.com>
2023-11-19 00:16:07 +0530

    Path update

--- components/Sections/About.tsx
+++ components/Sections/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AnimateOnScroll from "../AnimateOnScroll/AnimateOnScroll";
 import Image from "next/image";
-import Profile from "@/public/Profile.jpg";
+import Profile from "@/public/images/Profile.jpg";
 
 type Props = {};
 
