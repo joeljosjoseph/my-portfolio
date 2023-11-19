1a23bf3dfca94e2486cdc9378c8e988cbd03034f
joeljosjoseph <joeljosjoseph12@gmail.com>
2023-11-19 13:07:38 +0530

    Moved constants to separate file

---
+++ public/constants/constants.js
@@ -0,0 +1,16 @@
+export const GITHUB = "https://github.com/joeljosjoseph";
+export const LINKED_IN = "https://www.linkedin.com/in/joeljosjoseph/";
+export const TWITTER = "https://twitter.com/JoelJoz3";
+export const FACEBOOK = "https://www.facebook.com/Joel.Joz.Joseph";
+export const INSTAGRAM = "https://www.instagram.com/joeljosjoseph/";
+export const TIC_TAC_PROJECT = "https://github.com/joeljosjoseph/tictactoe";
+export const CATERING_PROJECT =
+  "https://github.com/joeljosjoseph/Catering-Management-System";
+
+export const SOCIAL_MEDIA_PROJECT =
+  "https://github.com/joeljosjoseph/Social-Media-Website";
+export const NETFLIX_PROJECT = "https://github.com/joeljosjoseph/Netflix-clone";
+export const WHATSAPP_PROJECT =
+  "https://github.com/joeljosjoseph/whatsapp-clone";
+export const LOTTERY_PROJECT =
+  "https://github.com/joeljosjoseph/hardhat-lottery";
